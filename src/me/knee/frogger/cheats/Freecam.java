7c84fa49cb6c52a12e7a3542ed1f6ed8ac9c42ae
Kneesnap <davidmenlo2@gmail.com>
2017-11-11 01:25:41 -0800

    Start improving freecam again.

--- src/me/knee/frogger/cheats/Freecam.java
+++ src/me/knee/frogger/cheats/Freecam.java
@@ -16,8 +16,8 @@ import static org.jnativehook.keyboard.NativeKeyEvent.*;
  * Allows freeform camera movement.
  * TODO: Reset camera position when player dies or level loads
  * TODO: Allow setting frogger's position to camera.
- * TODO: Fix jitter (Run right after the gametick occurs)
  * TODO: Remove culling
+ * TODO: Flip Camera XZ movements when Y is facing upwards.
  *
  * Mouse Up = Target Farther on the axis you're facing
  * Mouse Down = Target Closer on the axis you're facing
@@ -61,26 +61,28 @@ public class Freecam extends Cheat {
         int rotation = readByte(0x497214);
         for (Offset o : Offset.values()) {
             int val = o.keyMove(rotation);
-            zero(o.getOffset(), 4);
-            setInt(o.getOffset(), val);
+            zero(o.getOffset(), o.isTarget() ? 2 : 4);
+            set(o.getOffset(), val);
         }
     }
 
     @AllArgsConstructor @Getter
     private enum Offset {
-        CAMERA_X(0x497164, VC_A, VC_D, 2),
-        CAMERA_Y(0x497168, VC_SPACE, VC_SHIFT),
-        CAMERA_Z(0x49716C, VC_S, VC_W, 0),
-        TARGET_X(0x497144, VC_J, VC_L, 4),
-        TARGET_Z(0x497148, VC_K, VC_I, 3);
+        CAMERA_X(0x497164, VC_A, VC_D, 25, 2), // ca_current
+        CAMERA_Y(0x497168, VC_SPACE, VC_SHIFT, 25),
+        CAMERA_Z(0x49716C, VC_S, VC_W, 25, 0),
+        TARGET_X(0x497144, VC_J, VC_L, 7, 4), // ca_next_target_ofs
+        TARGET_Y(0x497146, VC_K, VC_I, -7),
+        TARGET_Z(0x497148, VC_1, VC_2, 7, 3);
 
         private final int offset;
         private final int decrKey;
         private final int incrKey;
+        private final int valueChange;
         private final int alternate;
 
-        Offset(int offset, int dKey, int iKey) {
-            this(offset, dKey, iKey, -1);
+        Offset(int offset, int dKey, int iKey, int valueChange) {
+            this(offset, dKey, iKey, valueChange, -1);
         }
 
         public int keyMove(int rotation) {
@@ -103,13 +105,19 @@ public class Freecam extends Cheat {
             if (isGamePaused())
                 return offset;
 
-            int valueChange = ordinal() >= 3 ? 50 : 25;
             int newValue = offset + (Main.isKeyPressed(iKey) ? valueChange : 0) - (Main.isKeyPressed(dKey) ? valueChange : 0);
+            if (isTarget())
+                newValue = (short) newValue; // Camera values are only shorts.
+
             if (Math.abs(newValue) < valueChange && Math.abs(offset) >= valueChange && o == TARGET_Z) // Stop camera before it would break bounds and enter a loop where its controls make the screen stuck.
                 return offset;
 
             map.put(this, newValue);
             return newValue;
+        }
+
+        public boolean isTarget() {
+            return ordinal() >= TARGET_X.ordinal();
         }
     }
 }
